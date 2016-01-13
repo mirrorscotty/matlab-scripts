baf681cf73369f46accf1463919a512e77e00868
Alex Griessman <alex.griessman@gmail.com>
2016-01-13 12:21:04 -0500

    Added a purely linear regression model.

--- data_analysis.m
+++ data_analysis.m
@@ -1,5 +1,5 @@
 %data = csvread('data-test2.csv', 1, 1);
-data = csvread('data_40-50-60-80_revised.csv', 1, 2);
+data = csvread('data_40-50-60-80-wVolume.csv', 1, 2);
 
 %Remove Outliers
 data([11 41 29],:) = [];
@@ -11,6 +11,15 @@ Ai = data(:,5);
 Af = data(:,6);
 ti = data(:,8);
 tf = data(:,9);
+Vsolid = data(:,16);
+Vwater = data(:,17);
+
+%T = (T-mean(T))/std(T);
+Vsolid = (Vsolid-mean(Vsolid))/std(Vsolid);
+Vwater = (Vwater-mean(Vwater))/std(Vwater);
+%tf = (tf-mean(tf))/std(tf);
+Vsolid = -1*Vsolid;
+Vwater = -1*Vwater;
 
 % Full model
 y = Af./Ai;
@@ -18,7 +27,7 @@ X = [T, T.^2, Xi, Xi.^2, Xf, Xf.^2, ti, ti.^2, tf, tf.^2];
 sf = regstats(y, X, 'linear', 'all');
 [b, bint] = regress(y, [ones(length(T), 1), X]);
 
-fprintf('Full Model\t R^2 = %g\n', sf.rsquare);
+fprintf('\n\nFull Model\t R^2 = %g\n', sf.rsquare);
 fprintf('Intercept\tb\tbint-\tbint+\tp-value\n');
 fprintf('Constant \t%.4g\t%.4g\t%.4g\t%g\n', b(1), bint(1,1), bint(1,2), sf.tstat.pval(1));
 fprintf('T        \t%.4g\t%.4g\t%.4g\t%g\n', b(2), bint(2,1), bint(2,2), sf.tstat.pval(2));
@@ -32,13 +41,13 @@ fprintf('ti^2     \t%.4g\t%.4g\t%.4g\t%g\n', b(9), bint(9,1), bint(9,2), sf.tsta
 fprintf('tf       \t%.4g\t%.4g\t%.4g\t%g\n', b(10), bint(10,1), bint(10,2), sf.tstat.pval(10));
 fprintf('tf^2     \t%.4g\t%.4g\t%.4g\t%g\n', b(11), bint(11,1), bint(11,2), sf.tstat.pval(11));
 
-% Full model
+% Reduced model
 y = sqrt(Af./Ai);
 X = [T, T.^2, Xf-Xi, (Xf-Xi).^2, tf];
 sr = regstats(y, X, 'linear', 'all');
 [b, bint] = regress(y, [ones(length(T), 1), X]);
 
-fprintf('Reduced Model\t R^2 = %g\n', sr.rsquare);
+fprintf('\nReduced Model\t R^2 = %g\n', sr.rsquare);
 fprintf('Intercept\tb\tbint-\tbint+\tp-value\n');
 fprintf('Constant \t%.4g\t%.4g\t%.4g\t%.4g\n', b(1), bint(1,1), bint(1,2), sr.tstat.pval(1));
 fprintf('T        \t%.4g\t%.4g\t%.4g\t%g\n', b(2), bint(2,1), bint(2,2), sr.tstat.pval(2));
@@ -47,4 +56,44 @@ fprintf('Xf-Xi    \t%.4g\t%.4g\t%.4g\t%g\n', b(4), bint(4,1), bint(4,2), sr.tsta
 fprintf('(Xf-Xi)^2\t%.4g\t%.4g\t%.4g\t%g\n', b(5), bint(5,1), bint(5,2), sr.tstat.pval(5));
 fprintf('tf       \t%.4g\t%.4g\t%.4g\t%g\n', b(6), bint(6,1), bint(6,2), sr.tstat.pval(6));
 
-[extr, loc] = max(sr.studres)
+% Linear Temperature model
+y = sqrt(Af./Ai);
+X = [T, Xf-Xi, (Xf-Xi).^2, tf];
+sr = regstats(y, X, 'linear', 'all');
+[b, bint] = regress(y, [ones(length(T), 1), X]);
+
+fprintf('\nLinear Temperature Model\t R^2 = %g\n', sr.rsquare);
+fprintf('Intercept\tb\tbint-\tbint+\tp-value\n');
+fprintf('Constant \t%.4g\t%.4g\t%.4g\t%.4g\n', b(1), bint(1,1), bint(1,2), sr.tstat.pval(1));
+fprintf('T        \t%.4g\t%.4g\t%.4g\t%g\n', b(2), bint(2,1), bint(2,2), sr.tstat.pval(2));
+fprintf('Xf-Xi      \t%.4g\t%.4g\t%.4g\t%g\n', b(3), bint(3,1), bint(3,2), sr.tstat.pval(3));
+fprintf('(Xf-Xi)^2    \t%.4g\t%.4g\t%.4g\t%g\n', b(4), bint(4,1), bint(4,2), sr.tstat.pval(4));
+fprintf('tf\t%.4g\t%.4g\t%.4g\t%g\n', b(5), bint(5,1), bint(5,2), sr.tstat.pval(5));
+
+% Linear model
+y = sqrt(Af./Ai);
+X = [T, Xf-Xi, tf];
+sr = regstats(y, X, 'linear', 'all');
+[b, bint] = regress(y, [ones(length(T), 1), X]);
+
+fprintf('\nLinear Model\t R^2 = %g\n', sr.rsquare);
+fprintf('Intercept\tb\tbint-\tbint+\tp-value\n');
+fprintf('Constant \t%.4g\t%.4g\t%.4g\t%.4g\n', b(1), bint(1,1), bint(1,2), sr.tstat.pval(1));
+fprintf('T        \t%.4g\t%.4g\t%.4g\t%g\n', b(2), bint(2,1), bint(2,2), sr.tstat.pval(2));
+fprintf('Xf-Xi      \t%.4g\t%.4g\t%.4g\t%g\n', b(3), bint(3,1), bint(3,2), sr.tstat.pval(3));
+fprintf('tf\t%.4g\t%.4g\t%.4g\t%g\n', b(4), bint(4,1), bint(4,2), sr.tstat.pval(4));
+
+% Volume Model
+y = Vsolid;
+X = [T.^2, Vwater, Vwater.^2, tf];
+sv = regstats(y, X, 'linear', 'all');
+[b, bint] = regress(y, [ones(length(T), 1), X]);
+
+fprintf('\nVolume Model\t R^2 = %g\n', sr.rsquare);
+fprintf('Intercept\tb\tbint-\tbint+\tp-value\n');
+fprintf('Constant \t%.4g\t%.4g\t%.4g\t%.4g\n', b(1), bint(1,1), bint(1,2), sv.tstat.pval(1));
+fprintf('T^2        \t%.4g\t%.4g\t%.4g\t%g\n', b(2), bint(2,1), bint(2,2), sv.tstat.pval(2));
+fprintf('Vwater      \t%.4g\t%.4g\t%.4g\t%g\n', b(3), bint(3,1), bint(3,2), sv.tstat.pval(3));
+fprintf('Vwater^2    \t%.4g\t%.4g\t%.4g\t%g\n', b(4), bint(4,1), bint(4,2), sv.tstat.pval(4));
+fprintf('tf       \t%.4g\t%.4g\t%.4g\t%g\n', b(5), bint(5,1), bint(5,2), sv.tstat.pval(5));
+%fprintf('tf       \t%.4g\t%.4g\t%.4g\t%g\n', b(6), bint(6,1), bint(6,2), sr.tstat.pval(6));
